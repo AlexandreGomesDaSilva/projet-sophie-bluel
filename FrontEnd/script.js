d1164be29a4571c8fbc86a5d0132b9f088ee9d24
AlexandreGomesDaSilva <alexgds92@gmail.com>
2025-01-16 01:58:34 +0100

    Dynamic display of all works

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -8,3 +8,19 @@ console.log(worksResponse);
 const categories = await fetch("http://localhost:5678/api/categories");
 const categoriesResponse = await categories.json();
 console.log(categoriesResponse);
+
+// Function to display jobs
+const displayWorks = (works) => {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML = works
+    .map((work) => {
+      return `<figure>
+        <img src="${work.imageUrl}" alt="${work.title}" />
+        <figcaption>${work.title}</figcaption>
+      </figure>`;
+    })
+    .join("");
+};
+
+// Display all jobs
+displayWorks(worksResponse);
